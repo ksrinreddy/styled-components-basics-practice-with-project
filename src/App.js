fbb73edb432feb045fa85b0d4576062f356bb683
ksrinreddy <ksrinreddy@gmail.com>
2023-05-24 19:17:30 +0530

    working with theme provider, themes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import React from "react";
 import Header from "./components/Header";
+import { ThemeProvider } from "styled-components";
 import { StyledContainer } from "./components/styles/Container.styled";
+
+// theme can be placed in seperate file if its large.
+const theme = {
+  colors: {
+    header: "#ebfbff",
+    body: "#fff",
+    footer: "003333",
+  },
+};
 
 const App = () => {
   return (
-    <>
-      <Header />
-      <StyledContainer>App</StyledContainer>
-    </>
+    <ThemeProvider theme={theme}>
+      <>
+        <Header />
+        <StyledContainer>App</StyledContainer>
+      </>
+    </ThemeProvider>
   );
 };
 
--- src/components/styles/Header.styled.js
+++ src/components/styles/Header.styled.js
@@ -3,7 +3,7 @@ import { styled } from "styled-components";
 export const StyledHeader = styled.header`
   //   background: #ebfbff;
   //   background: ${(props) => props.bg};
-  background: ${({ bg }) => bg};
+  background: ${({ theme }) => theme.colors.header};
   padding: 2.5rem 0;
 
   h1 {
